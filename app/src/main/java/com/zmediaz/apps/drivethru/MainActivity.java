430b44bc0b6c6a06240bb511f155b8f9ac7d1050
g63solutions <g63solutions@gmail.com>
2016-12-29 14:35:11 -0500

    Networking Grabbing API Menu SetTitile

--- app/src/main/java/com/zmediaz/apps/drivethru/MainActivity.java
+++ app/src/main/java/com/zmediaz/apps/drivethru/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private String mSelector;
     private MenuItem mMenuItem;
 
+    private TextView mErrorMessageDisplay;
+
+    private ProgressBar mLoadingIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         mMainTV = (TextView) findViewById(R.id.tv_display);
         mKey = getString(R.string.api);
         mSelector = "popular";
+
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
         loadMain();
     }
@@ -104,6 +114,12 @@ public class MainActivity extends AppCompatActivity {
     * is a params array that's why it has 0 the first item in array. You return to the post execute*/
     public class getHttpTask extends AsyncTask<URL, Void, String> {
         @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected String doInBackground(URL... params) {
             URL searchUrl = params[0];
             String jsonData = null;
@@ -118,9 +134,31 @@ public class MainActivity extends AppCompatActivity {
         //After thread has finished executing return from do in background goes here
         @Override
         protected void onPostExecute(String jsonData) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
             if (jsonData != null && !jsonData.equals("")) {
+                showJsonDataView();
                 mMainTV.setText(jsonData);
+            } else {
+                showErrorMessage();
             }
+        }
+
+        private void showJsonDataView() {
+            // First, make sure the error is invisible
+            mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+            // Then, make sure the JSON data is visible
+            mMainTV.setVisibility(View.VISIBLE);
+        }
+
+        private void showErrorMessage() {
+            // First, hide the currently visible data
+            mMainTV.setVisibility(View.INVISIBLE);
+            // Then, show the error
+            mErrorMessageDisplay.setVisibility(View.VISIBLE);
         }
     }
 }
+
+
+//Create error message that shows if data is not null or empty
+//Create Progress Bar that is initially invisible and shows as soon as thread is finished.
